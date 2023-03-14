59dd2df94d384364a63d2fd8366e6e76d416d215
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-03-14 19:42:49 +0000

    Deploying to gh-pages from @ Olga-Dames/goit-js-hw-09@6e0f404124bff5f3e8df4d654194ced99405beb4 🚀

--- 01-color-switcher.005553a3.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.start.addEventListener("click",(()=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.start.setAttribute("disabled",!0)})),t.stop.addEventListener("click",(()=>{clearInterval(e),t.start.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.005553a3.js.map
